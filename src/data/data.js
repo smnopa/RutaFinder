533d28135c372b8562068ef61c611e13f9d3de59
smnopa <104052195+smnopa@users.noreply.github.com>
2024-11-21 00:22:29 -0500

    Se agrega el data

---
+++ src/data/data.js
@@ -0,0 +1,77 @@
+export const ciudades = [
+  {
+    ciudad: "Bogotá",
+    hoteles: [
+      {
+        id: "hotel-1",
+        nombre: "Hotel Bogotá Plaza",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel de lujo con spa y restaurante.",
+      },
+      {
+        id: "hotel-2",
+        nombre: "Hotel Estelar",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel moderno cerca del aeropuerto.",
+      },
+      {
+        id: "hotel-2",
+        nombre: "Hotel Estelar",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel moderno cerca del aeropuerto.",
+      },
+      {
+        id: "hotel-2",
+        nombre: "Hotel Estelar",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel moderno cerca del aeropuerto.",
+      },
+    ],
+  },
+  {
+    ciudad: "Cartagena",
+    hoteles: [
+      {
+        id: "hotel-3",
+        nombre: "Hotel Caribe",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel frente al mar con piscina.",
+      },
+      {
+        id: "hotel-4",
+        nombre: "Hotel Las Americas",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Resort con acceso directo a la playa.",
+      },
+    ],
+  },
+  {
+    ciudad: "Bogotá",
+    hoteles: [
+      {
+        id: "hotel-1",
+        nombre: "Hotel Bogotá Plaza",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel de lujo con spa y restaurante.",
+      },
+      {
+        id: "hotel-2",
+        nombre: "Hotel Estelar",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel moderno cerca del aeropuerto.",
+      },
+      {
+        id: "hotel-2",
+        nombre: "Hotel Estelar",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel moderno cerca del aeropuerto.",
+      },
+      {
+        id: "hotel-2",
+        nombre: "Hotel Estelar",
+        img: "/img/Cartagena.jpg",
+        descripcion: "Hotel moderno cerca del aeropuerto.",
+      },
+    ],
+  },
+];
