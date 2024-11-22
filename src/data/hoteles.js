15782db6ca6a632c1bd82f0fd8f95934094ce5d3
smnopa <104052195+smnopa@users.noreply.github.com>
2024-11-22 18:42:14 -0500

    Se agregan nuevas funciones

--- src/data/hoteles.js
+++ src/data/hoteles.js
@@ -3,13 +3,13 @@ export const ciudades = [
     nombre: "Cartagena",
     hoteles: [
       {
-        id: "hotel-3",
+        id: "hotel-caribe",
         nombre: "Hotel Caribe",
         img: "/img/cartagena.png",
         descripcion: "Hotel frente al mar con piscina.",
       },
       {
-        id: "hotel-4",
+        id: "hotel-las-americas",
         nombre: "Hotel Las Americas",
         img: "/img/cartagena.png",
         descripcion: "Resort con acceso directo a la playa.",
@@ -20,74 +20,16 @@ export const ciudades = [
     nombre: "Bogotá",
     hoteles: [
       {
-        id: "hotel-1",
+        id: "hotel-bogota-plaza",
         nombre: "Hotel Bogotá Plaza",
         img: "/img/cartagena.png",
         descripcion: "Hotel de lujo con spa y restaurante.",
       },
       {
-        id: "hotel-2",
+        id: "hotel-estelar",
         nombre: "Hotel Estelar",
         img: "/img/cartagena.png",
         descripcion: "Hotel moderno cerca del aeropuerto.",
-      },
-      {
-        id: "hotel-2",
-        nombre: "Hotel Estelar",
-        img: "/img/cartagena.png",
-        descripcion: "Hotel moderno cerca del aeropuerto.",
-      },
-      {
-        id: "hotel-2",
-        nombre: "Hotel Estelar",
-        img: "/img/cartagena.png",
-        descripcion: "Hotel moderno cerca del aeropuerto.",
-      },
-    ],
-  },
-  {
-    nombre: "Bogotá",
-    hoteles: [
-      {
-        id: "hotel-1",
-        nombre: "Hotel Bogotá Plaza",
-        img: "/img/cartagena.png",
-        descripcion: "Hotel de lujo con spa y restaurante.",
-      },
-      {
-        id: "hotel-2",
-        nombre: "Hotel Estelar",
-        img: "/img/cartagena.png",
-        descripcion: "Hotel moderno cerca del aeropuerto.",
-      },
-      {
-        id: "hotel-2",
-        nombre: "Hotel Estelar",
-        img: "/img/cartagena.png",
-        descripcion: "Hotel moderno cerca del aeropuerto.",
-      },
-      {
-        id: "hotel-2",
-        nombre: "Hotel Estelar",
-        img: "/img/cartagena.png",
-        descripcion: "Hotel moderno cerca del aeropuerto.",
-      },
-    ],
-  },
-  {
-    nombre: "Cartagena",
-    hoteles: [
-      {
-        id: "hotel-3",
-        nombre: "Hotel Caribe",
-        img: "/img/cartagena.png",
-        descripcion: "Hotel frente al mar con piscina.",
-      },
-      {
-        id: "hotel-4",
-        nombre: "Hotel Las Americas",
-        img: "/img/cartagena.png",
-        descripcion: "Resort con acceso directo a la playa.",
       },
     ],
   },
