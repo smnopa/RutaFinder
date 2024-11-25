a37ca3bb6c1e2193e370da88071b1d0ef63b9a8e
smnopa <104052195+smnopa@users.noreply.github.com>
2024-11-25 16:41:06 -0500

    add: mapa

--- src/data/hoteles.js
+++ src/data/hoteles.js
@@ -7,6 +7,8 @@ export const ciudades = [
         nombre: "Hotel Caribe",
         img: "/img/cartagena.png",
         descripcion: "Hotel frente al mar con piscina.",
+        descripciongenral: ["sfhskdjlfhsdkjfsd","djlkasjdlaskdlas","jdlaskdjklasdjklasd"],
+        mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1m3976.774438095018!2d-75.1750!3d6.2225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f77db70d318f1%3A0xd758e0f59f1ae42b!2sLa%20Piedra%20del%20Pe%C3%B1ol!5e0!3m2!1sen!2sco!4v1646330764399!5m2!1sen!2sco",
       },
       {
         id: "hotel-las-americas",
