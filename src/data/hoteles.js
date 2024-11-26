c8cd8d26a300c7ebe44059a0f316e65251460e83
Seday2003 <sebastianipucchevi@gmail.com>
2024-11-26 11:09:19 -0600

    Eliminando residuos

--- src/data/hoteles.js
+++ src/data/hoteles.js
@@ -1,3 +1,5 @@
+import HistoriaCultura from "../pages/HistoriaCultura.astro";
+
 export const ciudades = [
   {
     nombre: "Cartagena",
@@ -19,6 +21,8 @@ export const ciudades = [
                               "Español, Inglés.",
                               "Incluye el servicio de recogida en tu hotel en la zona de El Poblado. El tiempo máximo de espera en el hotel es de 5 minutos."],
         mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.4834565845715!2d-75.55579808471768!3d10.398763292568846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef62e5cbec569b7%3A0x104f74f7c27e5db0!2sHotel%20Caribe%20By%20Faranda%20Grand!5e0!3m2!1sen!2sco!4v1701183437394!5m2!1sen!2scohttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.4834565845715!2d-75.55579808471768!3d10.398763292568846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef62e5cbec569b7%3A0x104f74f7c27e5db0!2sHotel%20Caribe%20By%20Faranda%20Grand!5e0!3m2!1sen!2sco!4v1701183437394!5m2!1sen!2scohttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.726057678473!2d-75.531659!3d10.403060!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef62e4e2a1a61f3%3A0x5d08edb25c0bc177!2sCra.%201%20%232–87%2C%20Cartagena%20de%20Indias%2C%20Provincia%20de%20Cartagena%2C%20Bol%C3%ADvar!5e0!3m2!1sen!2sco!4v1701184871450!5m2!1sen!2sco",
+        Historiacultura: []
+      
       },
       {
         id: "hotel-las-americas",
