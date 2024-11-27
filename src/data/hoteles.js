61ec64854b1fd4c8147bae590c41631bee61e603
smnopa <104052195+smnopa@users.noreply.github.com>
2024-11-26 20:12:43 -0500

    add: hoteles

--- src/data/hoteles.js
+++ src/data/hoteles.js
@@ -130,48 +130,162 @@ export const ciudades = [
           "/img/hotelGHLCoralesdeIndia4.jpg",
         ],
         descripciongeneral: [
-          "Cancelación gratuita",
-          "Reservar ahora y pagar después",
-          "Estadía 10.5 horas (con capacidad de extención)",
-          "Botones y servivio al cuarto",
-          "Servicio de recogida opcional",
+          "Piscina infinita con vista al mar",
+          "Desayuno incluido",
+          "Spa y centro de bienestar",
+          "Wi-Fi gratuito",
+          "Centro de negocios",
         ],
         descripciondetalles: [
-          "Cancela con hasta 24 horas de antelación y recibe un reembolso completo.",
-          "Planes flexibles: reserva tu plaza de inmediato, sin que se te haga el cargo.",
-          "Comprueba la disponibilidad para ver los horarios de inicio.",
-          "Español, Inglés.",
-          "Incluye el servicio de recogida en tu hotel en la zona de El Poblado. El tiempo máximo de espera en el hotel es de 5 minutos.",
+          "Desayuno buffet internacional con platos locales y opciones saludables.",
+          "Piscinas para adultos y niños con áreas recreativas.",
+          "Spa con sauna y masajes especializados.",
+          "Servicio de traslado al aeropuerto bajo solicitud.",
+          "Salones para eventos y convenciones con tecnología avanzada.",
         ],
-        mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.4834565845715!2d-75.55579808471768!3d10.398763292568846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef62e5cbec569b7%3A0x104f74f7c27e5db0!2sHotel%20Caribe%20By%20Faranda%20Grand!5e0!3m2!1sen!2sco!4v1701183437394!5m2!1sen!2scohttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.4834565845715!2d-75.55579808471768!3d10.398763292568846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef62e5cbec569b7%3A0x104f74f7c27e5db0!2sHotel%20Caribe%20By%20Faranda%20Grand!5e0!3m2!1sen!2sco!4v1701183437394!5m2!1sen!2scohttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.726057678473!2d-75.531659!3d10.403060!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef62e4e2a1a61f3%3A0x5d08edb25c0bc177!2sCra.%201%20%232–87%2C%20Cartagena%20de%20Indias%2C%20Provincia%20de%20Cartagena%2C%20Bol%C3%ADvar!5e0!3m2!1sen!2sco!4v1701184871450!5m2!1sen!2sco",
+        mapa: "https://www.google.com/maps/embed?...",
         contentData: [
           {
-            titulo: "Historia y Cultura del Hotel Caribe en Cartagena Colombia",
+            titulo: "Relájate en el GHL Corales de Indias",
             description:
-              "El Hotel Caribe en Cartagena es un hito histórico y cultural que refleja la esencia de la ciudad. A continuacion te presentamos una breve descripción de su historia, cultura y algunos datos curiosos:",
+              "GHL Corales de Indias es un destino ideal tanto para viajes de negocios como de placer, ofreciendo el equilibrio perfecto entre comodidad, lujo y ubicación privilegiada.",
             historia: {
               title: "Historia",
               content:
-                "Inaugurado en 1945, el Hotel Caribe fue uno de los primeros hoteles de lujo en Cartagena, diseñado para atraer tanto a turistas nacionales como internacionales. A lo largo de los años, el hotel ha sido testigo de la evolución de la ciudad y ha hospedado a importantes personalidades de la política, el entretenimiento y los negocios. Su arquitectura, que mezcla el estilo colonial con elementos modernos, ha sido restaurada y mantenida para preservar su encanto histórico. Durante más de 70 años, el Hotel Caribe ha sido un símbolo de hospitalidad y lujo en la costa caribeña colombiana.",
+                "Inaugurado en 2014, el hotel ha sido una opción popular por su ubicación estratégica cerca del aeropuerto y sus instalaciones modernas.",
             },
             cultura: {
               title: "Cultura",
               content:
-                "El Hotel Caribe es parte integral de la cultura cartagenera, no solo por su historia como uno de los primeros grandes hoteles de la ciudad, sino también por su vinculación con eventos importantes. A lo largo de los años, ha sido escenario de encuentros sociales, empresariales y culturales, promoviendo la difusión del arte, la música y la gastronomía de la región. Además, su cercanía al centro histórico de Cartagena permite a los huéspedes sumergirse en la vibrante cultura local, que abarca desde su arquitectura colonial hasta sus tradiciones gastronómicas.",
+                "Este hotel fomenta la cultura local ofreciendo eventos temáticos y colaboraciones con artistas de Cartagena.",
             },
             mitos: {
               title: "Datos Curiosos",
               content:
-                "El Hotel Caribe se distingue por su ubicación privilegiada en el barrio de Bocagrande, una de las zonas más exclusivas y turísticas de Cartagena, conocida por sus playas y ambiente moderno. Su arquitectura histórica, que combina elementos coloniales con detalles contemporáneos, ha sido cuidadosamente preservada. Además, ofrece acceso directo a la playa, permitiendo a los huéspedes disfrutar del mar Caribe sin salir del hotel. A lo largo de los años, el hotel ha acogido a celebridades, políticos y artistas internacionales, convirtiéndose en un referente de lujo y hospitalidad en la ciudad. Su oferta gastronómica también es de renombre, con restaurantes que destacan los sabores del Caribe colombiano.",
+                "El nombre 'Corales de Indias' se debe a los antiguos arrecifes que alguna vez protegieron la costa de Cartagena, visible desde las habitaciones.",
             },
             images: [
-              "/img/hotelCaribeH1.webp",
-              "/img/hotelCaribeH2.webp",
-              "/img/hotelCaribeH3.webp",
+              "/img/ghlCoralesH1.webp",
+              "/img/ghlCoralesH2.webp",
+              "/img/ghlCoralesH3.webp",
             ],
           },
         ],
-        flyer: "/img/FlyerHotelCaribe.jpg",
+        flyer: "/img/FlyerGHLCorales.jpg",
+      },
+      {
+        id: "hotel-hilton-cartagena",
+        nombre: "Hilton Cartagena",
+        img: "/img/hotelHiltonCartagena.jpg",
+        descripcion:
+          "Ubicado en la Península El Laguito, el Hilton Cartagena ofrece impresionantes vistas al mar Caribe, con piscinas, playa privada y múltiples restaurantes que hacen de tu estadía una experiencia inolvidable.",
+        bento: [
+          "/img/hotelHiltonCartagena.jpg",
+          "/img/hotelHiltonCartagena1.jpg",
+          "/img/hotelHiltonCartagena2.jpg",
+          "/img/hotelHiltonCartagena3.jpg",
+          "/img/hotelHiltonCartagena4.jpg",
+        ],
+        descripciongeneral: [
+          "Cancelación gratuita",
+          "Reservar ahora y pagar después",
+          "Estadía flexible",
+          "Gimnasio y spa",
+          "Restaurantes y bares",
+        ],
+        descripciondetalles: [
+          "Cancela sin penalización hasta 48 horas antes de la llegada.",
+          "Reserva hoy sin necesidad de pago inmediato.",
+          "Español, Inglés y Portugués.",
+          "Acceso exclusivo a playa privada y múltiples piscinas.",
+          "Servicio de traslado al aeropuerto disponible por un costo adicional.",
+        ],
+        mapa: "https://www.google.com/maps/embed?...",
+        contentData: [
+          {
+            titulo: "Descubre el lujo en el Hilton Cartagena",
+            description:
+              "Este hotel combina el lujo moderno con el encanto tropical. A continuación, te presentamos más sobre su historia y servicios.",
+            historia: {
+              title: "Historia",
+              content:
+                "Desde su apertura en los años 80, Hilton Cartagena se ha destacado por su lujo y servicio. Ha sido renovado constantemente para mantener su estatus como uno de los mejores hoteles de la ciudad.",
+            },
+            cultura: {
+              title: "Cultura",
+              content:
+                "Es conocido por organizar eventos culturales y festivales gastronómicos que atraen a visitantes locales e internacionales.",
+            },
+            mitos: {
+              title: "Datos Curiosos",
+              content:
+                "El hotel cuenta con una de las piscinas más grandes de Cartagena, y su playa privada es un punto destacado para los huéspedes.",
+            },
+            images: [
+              "/img/hiltonCartagenaH1.webp",
+              "/img/hiltonCartagenaH2.webp",
+              "/img/hiltonCartagenaH3.webp",
+            ],
+          },
+        ],
+        flyer: "/img/FlyerHiltonCartagena.jpg",
+      },
+      {
+        id: "hotel-sofitel-legend-santa-clara",
+        nombre: "Sofitel Legend Santa Clara Cartagena",
+        img: "/img/hotelSofitelLegendSantaClara.jpg",
+        descripcion:
+          "Antiguo convento del siglo XVII, el Sofitel Santa Clara combina la historia colonial con el lujo moderno, ubicado en el corazón del centro histórico de Cartagena.",
+        bento: [
+          "/img/hotelSofitelLegendSantaClara.jpg",
+          "/img/hotelSofitelLegendSantaClara1.jpg",
+          "/img/hotelSofitelLegendSantaClara2.jpg",
+          "/img/hotelSofitelLegendSantaClara3.jpg",
+          "/img/hotelSofitelLegendSantaClara4.jpg",
+        ],
+        descripciongeneral: [
+          "Cancelación gratuita",
+          "Desayuno incluido",
+          "Spa de lujo",
+          "Piscina al aire libre",
+          "Tours personalizados",
+        ],
+        descripciondetalles: [
+          "Cancela con hasta 72 horas de antelación para reembolso completo.",
+          "Desayuno buffet de cortesía todas las mañanas.",
+          "Servicio de conserjería multilingüe.",
+          "Acceso a spa con tratamientos exclusivos.",
+          "Ofrecen tours privados por el centro histórico.",
+        ],
+        mapa: "https://www.google.com/maps/embed?...",
+        contentData: [
+          {
+            titulo: "Explora la historia y el lujo en el Sofitel Santa Clara",
+            description:
+              "Este hotel emblemático ofrece una mezcla única de arquitectura colonial y confort contemporáneo.",
+            historia: {
+              title: "Historia",
+              content:
+                "Originalmente construido como convento en 1621, fue restaurado y convertido en un hotel de lujo en los años 90, conservando su estilo colonial.",
+            },
+            cultura: {
+              title: "Cultura",
+              content:
+                "El hotel es famoso por sus exposiciones de arte y su participación en eventos culturales en Cartagena.",
+            },
+            mitos: {
+              title: "Datos Curiosos",
+              content:
+                "Algunas habitaciones tienen paredes originales del convento, y el hotel es conocido por sus leyendas sobre monjas y túneles secretos.",
+            },
+            images: [
+              "/img/sofitelSantaClaraH1.webp",
+              "/img/sofitelSantaClaraH2.webp",
+              "/img/sofitelSantaClaraH3.webp",
+            ],
+          },
+        ],
+        flyer: "/img/FlyerSofitelSantaClara.jpg",
       },
     ],
   },
