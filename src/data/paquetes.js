fcebfcd13768d094ca8d6dc2bb0b44f310475067
Seday2003 <sebastianipucchevi@gmail.com>
2024-12-06 02:16:51 -0600

    add info: SantaMarta

--- src/data/paquetes.js
+++ src/data/paquetes.js
@@ -1362,11 +1362,17 @@ export const paquetes = [
         descripciongeneral: [
           "Transporte ida y vuelta",
           "Entrada al Parque Tayrona",
-          "Guía especializado en biodiversidad"
+          "Guía especializado en biodiversidad",
+          "Camara profesional para capturar momentos inolvidables",
+          "Cancelacion Gratuita",
+          
         ],
         descripciondetalles: [
           "Caminata guiada hacia las playas más famosas como Cabo San Juan.",
-          "Incluye refrigerio y tiempo libre para nadar y explorar."
+          "Incluye refrigerio y tiempo libre para nadar y explorar.",
+          "Guía especializado en biodiversidad",
+          "Camara profesional para capturar momentos inolvidables.",
+          "Incluye el servicio de recogida en tu hotel en la zona de El Poblado. El tiempo máximo de espera en el hotel es de 5 minutos.",
         ],
         mapa: "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d250406.9733184275!2d-74.22900832211596!3d11.290129112530865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sTayrona%3A%20Playa%20y%20Naturaleza%20santa%20marta!5e0!3m2!1ses!2sco!4v1733414992970!5m2!1ses!2sco",
         flyer: "/img/FlyerpTayrona.jpg",
@@ -1399,11 +1405,16 @@ export const paquetes = [
         descripciongeneral: [
           "Expedición de 4 días",
           "Guías indígenas locales",
-          "Incluye alimentación y transporte"
+          "Incluye alimentación y transporte",
+          "Camara profesional para capturar momentos inolvidables",
+          "Cancelacion Gratuita",
         ],
         descripciondetalles: [
           "Caminata por senderos selváticos hasta la histórica Ciudad Perdida.",
-          "Oportunidad de interactuar con comunidades indígenas locales."
+          "Oportunidad de interactuar con comunidades indígenas locales.",
+          "Incluye alimentación y transporte",
+          "Camara profesional para capturar momentos inolvidables.",
+          "Incluye el servicio de recogida en tu hotel en la zona de El Poblado. El tiempo máximo de espera en el hotel es de 5 minutos.",
         ],
         mapa: "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d4044076.6723497906!2d-77.34661738814532!3d8.173145476421958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s%22Ciudad%20Perdida!5e0!3m2!1ses!2sco!4v1733415956525!5m2!1ses!2sco",
         flyer: "/img/FlyerCiudadPerdida.jpg",
@@ -1436,11 +1447,16 @@ export const paquetes = [
         descripciongeneral: [
           "Transporte ida y vuelta",
           "Entrada al Acuario Marino",
-          "Tiempo libre en el Rodadero"
+          "Tiempo libre en el Rodadero",
+          "Camara profesional para capturar momentos inolvidables",
+          "Cancelacion Gratuita",
         ],
         descripciondetalles: [
           "Incluye paseo en lancha hacia el acuario.",
-          "Demostraciones educativas sobre fauna marina y playas cercanas."
+          "Demostraciones educativas sobre fauna marina y playas cercanas.",
+          "Free time en el rodadero, aprovecha al maximo tu estadía.",
+          "Camara profesional para capturar momentos inolvidables.",
+          "Incluye el servicio de recogida en tu hotel en la zona de El Poblado. El tiempo máximo de espera en el hotel es de 5 minutos.",
         ],
         mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15654.814239023597!2d-74.24223550141465!3d11.20955492346643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef45f5a8a12f197%3A0x6916282e7135b834!2sAcuario%20Mundo%20Marino!5e0!3m2!1ses!2sco!4v1733416649038!5m2!1ses!2sco",
         flyer: "/img/FlyerRodadero.jpg",
@@ -1473,11 +1489,16 @@ export const paquetes = [
         descripciongeneral: [
           "Transporte ida y vuelta",
           "Visita a cascadas y finca cafetera",
-          "Almuerzo típico incluido"
+          "Almuerzo típico incluido",
+          "Camara profesional para capturar momentos inolvidables",
+          "Cancelacion Gratuita",
         ],
         descripciondetalles: [
           "Recorrido guiado por cascadas como Pozo Azul.",
-          "Degustación de café local y explicación del proceso de producción."
+          "Degustación de café local y explicación del proceso de producción.",
+          "Almuerzo típico de Minca.",
+          "Camara profesional para capturar momentos inolvidables.",
+          "Incluye el servicio de recogida en tu hotel en la zona de El Poblado. El tiempo máximo de espera en el hotel es de 5 minutos.",
         ],
         mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7829.208804932143!2d-74.12182680850569!3d11.142812006116323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef4f129819389d3%3A0x9f9e3a171b198b5!2sMinca%2C%20Santa%20Marta%2C%20Magdalena!5e0!3m2!1ses!2sco!4v1733416970420!5m2!1ses!2sco",
         flyer: "/img/FlyerpMinca.jpg",
@@ -1510,11 +1531,16 @@ export const paquetes = [
         descripciongeneral: [
           "Transporte ida y vuelta",
           "Almuerzo típico incluido",
-          "Guía turístico local"
+          "Guía turístico local",
+          "Camara profesional para capturar momentos inolvidables",
+          "Cancelacion Gratuita",
         ],
         descripciondetalles: [
           "Tiempo libre para disfrutar de la playa y actividades como snorkeling.",
-          "Opción de paseo en bote por la bahía."
+          "Opción de paseo en bote por la bahía.",
+          "Guia turístico local.",
+          "Camara profesional para capturar momentos inolvidables.",
+          "Incluye el servicio de recogida en tu hotel en la zona de El Poblado. El tiempo máximo de espera en el hotel es de 5 minutos.",
         ],
         mapa: "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d31300.37235843068!2d-74.16900179054264!3d11.294706288771845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1zQmFow61hIENvbmNoYTogUmVsw6FqYXRlIGVuIGVsIFBhcmHDrXNv!5e0!3m2!1ses!2sco!4v1733417303700!5m2!1ses!2sco",
         flyer: "/img/FlyerBahiaConcha.jpg",
@@ -1547,11 +1573,16 @@ export const paquetes = [
         descripciongeneral: [
           "Curso básico de buceo",
           "Equipos incluidos",
-          "Transporte ida y vuelta"
+          "Transporte ida y vuelta",
+          "Camara profesional para capturar momentos inolvidables",
+          "Cancelacion Gratuita",
         ],
         descripciondetalles: [
           "Incluye instrucción por profesionales certificados.",
-          "Oportunidad de explorar arrecifes y fauna marina de la región."
+          "Oportunidad de explorar arrecifes y fauna marina de la región.",
+          "Transporte ida y vuelta, con actividades de buceo y snorkeling.",
+          "Camara profesional para capturar momentos inolvidables.",
+          "Incluye el servicio de recogida en tu hotel en la zona de El Poblado. El tiempo máximo de espera en el hotel es de 5 minutos.",
         ],
         mapa: "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d31304.669947828846!2d-74.22267354058073!3d11.255249841127005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sTaganga%20y%20Buceo!5e0!3m2!1ses!2sco!4v1733417430700!5m2!1ses!2sco",
         flyer: "/img/FlyerTaganga.jpg",
